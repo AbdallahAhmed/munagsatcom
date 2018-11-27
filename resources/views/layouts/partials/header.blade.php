f76c7637a18e71c0b3af327faaad0e62e7a1ebee
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-11-27 20:25:09 +0200

    Add Register Footer

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -10,7 +10,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="{{asset('/assets')}}/images/logo.jpg" alt="{{trans('app.name')}}"></a>
+            <a class="navbar-brand" href="{{asset(app()->getLocale())}}"><img src="{{asset('/assets')}}/images/logo.jpg" alt="{{trans('app.name')}}"></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">

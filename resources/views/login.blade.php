adfe0ce9c7d5edf1cbf7c9397bcf7e52658535f4
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-12-03 10:46:03 +0200

    Add Errors handler

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,12 +17,13 @@
                 <form method="post" action="{{route('login')}}">
                     {{csrf_field()}}
                     <div class="form-group-lg row">
-                        <label class="col-xs-12 col-md-3">{{trans('app.fields.username')}} </label>
+                        <label class="col-xs-12 col-md-3">{{trans('app.fields.email')}} </label>
                         <div class="col-xs-12 col-md-9">
                             <div class="new-f-group">
                                 <div class="form-group clearfix">
                                     <span class="icony"><i class="fa fa-user"></i></span>
-                                    <input name="username" type="text" class="effect-9 form-control" placeholder="{{trans('app.fields.username')}}">
+                                    <input name="email" type="email" class="effect-9 form-control"
+                                           placeholder="{{trans('app.fields.email')}}">
                                     <span class="focus-border"><i></i></span>
                                 </div>
                             </div>
@@ -32,7 +33,8 @@
                         <label class="col-xs-12 col-md-3">{{trans('app.fields.password')}}</label>
                         <div class="new-f-group col-xs-12 col-md-9">
                             <div class="form-group">
-                                <span class="icony"><i class="fa fa-fw field-icon toggle-password fa-eye" toggle="#password-field"></i></span>
+                                <span class="icony"><i class="fa fa-fw field-icon toggle-password fa-eye"
+                                                       toggle="#password-field"></i></span>
                                 <input name="password" type="password" class="effect-9 form-control" id="password-field"
                                        placeholder="{{trans('app.fields.repassword')}}">
                                 <span class="focus-border"><i></i></span>
@@ -43,17 +45,26 @@
                         <label class="col-xs-12 col-md-3"></label>
                         <div class="col-xs-12 col-md-9">
                             <label class="radio-inline" id="input-user_type-personal"> {{trans('app.personal')}}</label>
-                            <input type="radio"   {{old('user_type')==1||!old('user_type')?'checked':''}} id="input-user_type-personal" name="user_type" value="1" onclick="show1();">
+                            <input type="radio"
+                                   {{old('user_type')==1||!old('user_type')?'checked':''}} id="input-user_type-personal"
+                                   name="user_type" value="1" onclick="show1();">
 
                             <label class="radio-inline" id="input-user_type-company">{{trans('app.company')}}</label>
-                            <input type="radio"   {{old('user_type')==2?'checked':''}} name="user_type" value="2"  id="input-user_type-company" onclick="show2();">
+                            <input type="radio" {{old('user_type')==2?'checked':''}} name="user_type" value="2"
+                                   id="input-user_type-company" onclick="show2();">
                         </div>
                     </div>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <div class="form-group-lg text-center">
                         <button type="submit" class="padding-md fbutcenter width"> دخول</button>
                     </div>

51d49a36636ce136613e01cb27f9c2ee32c69bce
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-12-13 19:05:39 +0200

    Messages Api

---
+++ database/migrations/2018_12_13_165331_create_channels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateChannelsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('channels', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->integer('sender_id');
+			$table->integer('receiver_id');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('channels');
+	}
+
+}

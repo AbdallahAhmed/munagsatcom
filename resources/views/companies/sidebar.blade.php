4e9bbe9c2d1cfcb83f342343bc6d319ad6d754e3
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-12-15 17:45:36 +0200

    updates

--- resources/views/companies/sidebar.blade.php
+++ resources/views/companies/sidebar.blade.php
@@ -2,33 +2,59 @@
     <div class="side-box">
         <div class="profile-side">
             <ul>
-                <li @if (\Route::current()->getName() == 'company.show') class="active" @endif><a class="" href="{{route('company.show', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.show') class="active" @endif><a class=""
+                                                                                                  href="{{route('company.show', ['id' => $company_id])}}">
                         {{trans('app.company_details')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.chances') class="active" @endif><a href="{{route('company.chances', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.chances') class="active" @endif><a
+                            href="{{route('company.chances', ['id' => $company_id])}}">
                         {{trans('app.chances.the_chances')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.tenders') class="active" @endif><a href="{{route('company.tenders', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.tenders') class="active" @endif><a
+                            href="{{route('company.tenders', ['id' => $company_id])}}">
                         {{trans('app.name')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.employees') class="active" @endif><a href="{{route('company.employees', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.employees') class="active" @endif><a
+                            href="{{route('company.employees', ['id' => $company_id])}}">
                         {{trans('app.employees')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.centers') class="active" @endif><a href="{{route('company.centers', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.centers') class="active" @endif><a
+                            href="{{route('company.centers', ['id' => $company_id])}}">
                         {{trans('app.centers.centers')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.requests') class="active" @endif><a href="{{route('company.requests', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.requests') class="active" @endif><a
+                            href="{{route('company.requests', ['id' => $company_id])}}">
                         {{trans('app.requests')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.employees.search') class="active" @endif><a href="{{route('company.employees.search', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.employees.search') class="active" @endif><a
+                            href="{{route('company.employees.search', ['id' => $company_id])}}">
                         {{trans('app.search_employee')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.add_delegate') class="active" @endif><a href="{{route('company.add_delegate', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.add_delegate') class="active" @endif><a
+                            href="{{route('company.add_delegate', ['id' => $company_id])}}">
                         {{trans('app.add_delegate')}}
                     </a></li>
-                <li @if (\Route::current()->getName() == 'company.messages') class="active" @endif><a href="{{route('company.messages', ['id' => $company_id])}}">
+                <li @if (\Route::current()->getName() == 'company.messages') class="active" @endif><a
+                            href="{{route('company.messages', ['id' => $company_id])}}">
                         {{trans('app.messages')}}
                     </a></li>
+                <?php
+                use App\Models\Companies_empolyees;
+                $role = [];
+                if (fauth()->check()) {
+                    $role = Companies_empolyees::where([
+                        ['employee_id', fauth()->user()->id],
+                        ['accepted', 1],
+                        ['status', 1]
+                    ])->get();
+                }
+                ?>
+                @if($role && $role[0]->role == 1)
+                    <li @if (\Route::current()->getName() == 'chances.create') class="active" @endif><a
+                                href="{{route('chances.create', ['id' => $company_id])}}">
+                            {{trans('app.add_chance')}}
+                        </a></li>
+                @endif
             </ul>
         </div>
     </div>

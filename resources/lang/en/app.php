2adab0e7b7df4cb6256c89ece030f0e55128f6db
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-12-27 16:58:16 +0200

    Chance apply logged in

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -101,6 +101,8 @@ return [
     'add_other_units' => 'Add new units',
     'add_center' => 'Add Center',
     'the_unit' => 'Unit',
+    'register_chance_apply' => 'You have to register to apply on this chance',
+    'login_chance_apply' => 'If you have an account, please sign in first using your email an password!',
     'tenders' => [
         'tenders' => 'Government tenders',
         "details" => 'Tender \'s details',

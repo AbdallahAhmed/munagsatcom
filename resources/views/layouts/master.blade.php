08e768107ed5a0428fcdc9b2fc6280968c3eece1
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2019-01-12 19:10:02 +0200

    Language test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{trans('app.name')}} | @yield("title")</title>
-    @if(app()->getLocale() == 'ar')
+    @if(app()->getLocale() != 'en')
     <link href="{{asset('/assets')}}/css/maincss.css" rel="stylesheet">
     @else
     <link href="{{asset('/assets')}}/css/maincss-en.css" rel="stylesheet">

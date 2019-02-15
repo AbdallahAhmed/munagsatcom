6bef4cc6903372a104ada8d8596014520ae35a6c
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2019-02-15 18:51:00 +0200

    Ttilee

--- app/Mail/VerificationMail.php
+++ app/Mail/VerificationMail.php
@@ -34,6 +34,6 @@ class VerificationMail extends Mailable
      */
     public function build()
     {
-        return $this->from('noreply@munagasatcom.com', 'Monaasat')->view('mail.verification', ['user' => $this->user]);
+        return $this->view('mail.verification', ['user' => $this->user]);
     }
 }

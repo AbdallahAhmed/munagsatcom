bbd592af54dab645e8114088eed37aacb0744ef2
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2019-03-06 07:19:53 +0200

    Contact us

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-sm-6 col-md-4 ">
                 <ul>
-                    <li><i class="fa fa-phone"></i> <a href="#">{{route('contact-us')}}</a></li>
+                    <li><i class="fa fa-phone"></i> <a href="{{route('contact-us')}}">{{trans('app.contact_us')}}</a></li>
                     <li><i class="fa fa-question"></i> <a href="{{route('page.show', ['slug' => 'الأسئلة-الشائعة'])}}">
                             {{trans('app.q_a')}}</a></li>
                     <li><i class="fa fa-mobile"></i> 920008769 </li>

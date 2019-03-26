b5b3da64a5165b754919f286b424e85cfe282005
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2019-03-26 12:05:04 +0200

    Chances

--- app/Models/Chance.php
+++ app/Models/Chance.php
@@ -29,7 +29,7 @@ class Chance extends \Dot\Chances\Models\Chance
     public function getProgressAttribute()
     {
         $now = Carbon::now();
-        $diff = ($this->closing_date->diffInHours($this->created_at));
+        $diff = max($this->closing_date->diffInHours($this->created_at),1);
         return max(min(((($diff - max($now->diffInHours($this->closing_date, false), 0)) / $diff) * 100), 100), 1);
     }
 

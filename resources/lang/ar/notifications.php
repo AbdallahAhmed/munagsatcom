1e1991816bab39b4146d9bef1b6ce45dfec459ad
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2019-04-30 14:45:34 +0200

    Fixed

--- resources/lang/ar/notifications.php
+++ resources/lang/ar/notifications.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'user.register' => 'تم إنشاء حسابك يرجى التحقق من كذا كذا',
+    'user.register' => 'تم إنشاء حسابك يرجى التحقق من بريدك الكترونى',
     'password.reset' => 'لقد تم تغير كلمة المرور الخاصة بك',
     'tender.bought' => 'لقد قمت بشراء مناقصة',
     'to.company.tender.bought' => 'قام بشراء مناقصة :user',
